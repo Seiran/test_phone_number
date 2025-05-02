7c5eaeb4f6b66a870f8a44b2d80e9bcc13ec555b
seiran <4947853+Seiran@users.noreply.github.com>
2025-05-02 18:14:55 +0300

    upd

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ $_SESSION['evinaRequestId'] = generateEvinaRequestId();
   <meta name="robots" content="noindex, nofollow">
   <meta name="googlebot" content="noindex">
   <meta name="referrer" content="no-referrer">
-  <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="format-detection" content="telephone=no">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
